674000b9b8d637dff8297d56411a8440e284161c
Darren Rainey <darren@darrenraineys.co.uk>
2016-12-02 12:18:31 +0000

    Create printf.c

---
+++ printf.c
@@ -0,0 +1,11 @@
+// Code By Darren Rainey 2016
+
+#include <stdio.h> // Import Standard Header Files
+
+int main() // Start main function
+{
+
+  printf("Hello\n"); // Print hello to the screen with a new line at the end
+
+  return 0; // Exit If Successful
+}
