1851e2cb213ee1c9ff40df4a93a4f08424dc4407
Darren Rainey <darren@darrenraineys.co.uk>
2016-12-02 12:21:58 +0000

    Create system.c

---
+++ system.c
@@ -0,0 +1,10 @@
+#include <stdio.h> // Include standard header files
+#include <stdio.h> // Include standard libarys to use system function
+
+int main() // Start main function
+{
+
+  system("echo SYSTEM COMMAND"); // runs echo SYSTEM COMMAND
+
+  return 0; // Exit if no errors
+]
