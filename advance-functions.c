ea026759dbff800eb5342242bf180c57099b11a4
Darren Rainey <darren@darrenraineys.co.uk>
2016-12-03 18:49:18 +0000

    Create advance-functions.c

---
+++ advance-functions.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Include standard header file
+
+int main(int argc, char* argv[]) // Allow arguments to be passed into program
+{
+  printf("Filename is %s\n",argv[0]); // Print argument 0 / Filename
+  printf("Argument 1 was %s\n",argv[1]); // Print the first command argument 
+  return 0; // Exit if no errors
+}
