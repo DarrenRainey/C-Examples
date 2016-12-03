f135caa85f8b7c591b054824da52c502b20b3f6c
Darren Rainey <darren@darrenraineys.co.uk>
2016-12-03 18:38:40 +0000

    Create basic-math.c

---
+++ basic-math.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // Include standard header files
+
+int main() // Start main function  
+{
+  int numbers[10]; // start numbers array
+  numbers[1] = 1; // set numbers to 1
+  numbers[3] = 3; // set numbers to 3
+
+  int total;
+  total = numbers[1] + numbers[3]; // Add Numbers
+
+  printf("Total %d\n",total); // Print Total
+
+  return 0; // Exit if no errors
+}
