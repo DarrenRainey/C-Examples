c2c5ec08ab20ac16fb1f94c625aa057aa1d5ae55
Darren Rainey <darren@darrenraineys.co.uk>
2016-12-04 14:44:45 +0000

    Create scanf.c

---
+++ scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // Include standard header files
+
+int main() // Start main function
+{
+        char buffer[20]; // Set a buffer char to temporay save data
+
+        printf("What is your name ?:  "); // Print a message asking for your name
+        scanf("%s",buffer); // Write the output of printf to the buffer char
+
+        printf("Hello %s\n",buffer); // Print hello with data from the buffer char
+        return 0; // exit if successful
+}
