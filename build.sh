06bc218fb143e0030599754454b91b74e47ec454
Darren Rainey <darren@darrenraineys.co.uk>
2016-12-05 13:19:34 +0000

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# C examples by DarrenRainey http://github.com/DarrenRainey
+mkdir build
+echo Building Advance Functions example
+gcc advance-functions.c -o build/advance-functions
+echo Building Basic Math example
+gcc basic-math.c -o build/basic-math
+echo Building Cross Platform example
+gcc cross-platform.c -o build/cross-platform
+echo Building Printf example
+gcc printf.c -o build/printf
+echo Building Scanf example
+gcc scanf.c -o build/scanf
+echo Building System example
+gcc system.c -o build/system
+echo ""
+echo Build Complete all builds saved to $PWD/build
