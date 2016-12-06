b389ccb4bf6c2a4fa5a1374d9909dd8f5d4cc3ee
Darren Rainey <darren@darrenraineys.co.uk>
2016-12-06 19:37:02 +0000

    Fixed Syntax Error

--- while-loops.c
+++ while-loops.c
@@ -7,7 +7,7 @@ int main() // Start main function
   int i = 0; // Set i as a integer and set it to zero
   while(i < 10) // Run while i is less than 10
   { // Start of while function
-  i++ // Add  1 to i
+  i++; // Add  1 to i
   printf("Looped %d Times\n",i); // Print current  number stored in i integer
   } // End of while function
 
