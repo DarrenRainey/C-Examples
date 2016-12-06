3da1ed9ad11baf9836ece0a8f5223cca1ecd00fc
Darren Rainey <darren@darrenraineys.co.uk>
2016-12-06 19:32:35 +0000

    Create while-loops.c

---
+++ while-loops.c
@@ -0,0 +1,15 @@
+// C while loop example by Darren Rainey http://github.com/DarrenRainey
+
+#include <stdio.h> // Include standard input and output header files
+
+int main() // Start main function
+{
+  int i = 0; // Set i as a integer and set it to zero
+  while(i < 10) // Run while i is less than 10
+  { // Start of while function
+  i++ // Add  1 to i
+  printf("Looped %d Times\n",i); // Print current  number stored in i integer
+  } // End of while function
+
+  return 0; // Exit if successful
+}
