7153035db41a26d775b93a722fab64249ec5fb1f
Darren Rainey <darren@darrenraineys.co.uk>
2016-12-08 10:15:58 +0000

    Create commnets.c

---
+++ commnets.c
@@ -0,0 +1,15 @@
+// C example by Darren Rainey http://github.com/DarrenRainey
+
+#include <stdio.h> // Include standard input and output header file
+
+int main() // Start main function
+{
+  // This is a single line comment and wont show up in the program
+  
+  */ This is a multiline comment
+    useful for lots of text
+  /* and this closes the comment
+
+
+  return 0;
+}
