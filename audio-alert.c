5c1e68553019b940a7621568362d3c4a573db4cf
Darren Rainey <darren@darrenraineys.co.uk>
2016-12-18 14:46:47 +0000

    Create audio-alert.c

---
+++ audio-alert.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // Include standard input and output header files
+
+int main() // Start Main Function
+{
+  printf("This will cause a audio beep\n\a"); // Print this message and cause a system beep
+
+  return 0; // Exit if successful
+}
