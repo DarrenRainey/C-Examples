b9952c4134604517ab04b0e95bf9c21788d42aed
Darren Rainey <darren@darrenraineys.co.uk>
2016-12-24 11:53:06 +0000

    Create printf-multiple-chars.c

---
+++ printf-multiple-chars.c
@@ -0,0 +1,13 @@
+// C example by DarrenRainey http://github.com/DarrenRainey
+
+#include <stdio.h> // Include standard input and output header  file
+
+int main()
+{
+  char 1[] =  "This Is the First Char"; // Set Char One
+  char 2[] =  "And This  Is The Second  Char"; // Set Char Two
+
+  printf("%s %s",1,2);  //  Print  the information stored in the two char's
+
+  return 0; // Exit if successful
+}
