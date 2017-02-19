9ad05026dccb017d5189afd39cdc43656a1004cf
Darren Rainey <darren@darrenraineys.co.uk>
2017-02-19 11:46:14 +0000

    Create storage-limits.c

---
+++ storage-limits.c
@@ -0,0 +1,15 @@
+//  C example by Darren Rainey  http://github.com/DarrenRainey
+#include <stdio.h>  // Include Standard Header File
+#include <limits.h> // Include Limits Header File
+
+int main()
+{
+  // Print the sizeof each variable type using the sizeof function at the end of the printf statment
+	printf("Total Storage Size Of INT In Bytes %d\n", sizeof(int));
+	printf("Total Storage Size Of CHAR In Bytes %d\n", sizeof(char));
+	printf("Total Storage Size Of LONG In Bytes %d\n", sizeof(long));
+	printf("Total Storage Size Of FLOAT In Bytes %d\n", sizeof(float));
+	printf("Total Storage Size Of UNSIGNED INT In Bytes %d\n", sizeof(unsigned int));
+	printf("Total Storage Size Of UNSIGNED CHAR In Bytes %d\n", sizeof(unsigned char));
+	return 0; // Exit if no errors
+}
