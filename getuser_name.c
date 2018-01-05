1f3217d9d401d1b5ccc28b79f211f7430d848f19
Darren Rainey <darren@darrenraineys.co.uk>
2018-01-05 18:28:26 +0000

    Update getuser_name.c

--- getuser_name.c
+++ getuser_name.c
@@ -1,11 +1,12 @@
 // C Example by Darren Rainey https://github.com/DarrenRainey/C-Examples
-#include <stdio.h> // Include standard C header file
-#include <stdlib.h> // Include standard libary header file
+#include <stdio.h> // Include standard C header file.
+#include <stdlib.h> // Include standard libary header file.
 
-int main(int argc, char *argv[]) // Start main function
+int main(int argc, char *argv[]) // Start main function.
 {
-        char *user = getenv("USER"); // Create a char called user and set its output to getenv user
-        printf("%s",user); // Print user name
+        // Note getenv output/input should be considered insecure and dangerous as eviroment variables can be edited by the current user.
+        char *user = getenv("USER"); // Create a char called user and set its output to getenv user.
+        printf("%s",user); // Print user name.
 
-        return 0; // Exit if no errors
+        return 0; // Exit if no errors.
 }
