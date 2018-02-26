a331968df65d2b6aed4e594ccee2eb52434fd25a
Darren Rainey <darren@darrenraineys.co.uk>
2018-02-26 19:10:15 +0000

    Create Clear-Console-Window.c

---
+++ Clear-Console-Window.c
@@ -0,0 +1,7 @@
+// C example code by darren rainey
+#include <stdio.h> // Include standard header file
+
+int main() // Start main function
+{
+        printf("\e[1;1H\e[2J"); // print special control character to reset/clear terminal
+}
