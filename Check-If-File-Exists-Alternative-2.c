d9bfc03f092ce93ac8801a168619bf3672653302
Darren Rainey <darren@darrenraineys.co.uk>
2018-03-03 16:44:00 +0000

    Create Check-If-File-Exists-Alternative-2.c

---
+++ Check-If-File-Exists-Alternative-2.c
@@ -0,0 +1,16 @@
+// C-example by Darren Rainey 2018 https://github.com/DarrenRainey/C-Examples
+#include <stdio.h> // Include standard C header file
+
+int main() // Start main function
+{
+  if(stat("file.txt") == 0) // If file.txt exists run the following code
+  {
+    printf("IT EXISTS\n"); // print it exists
+  }
+  else // If it doesn't exist
+  {
+    printf("IT DOES NOT EXIST\n") // print it does not exist
+  }
+  
+  return 0; // Exist succesfully
+}
