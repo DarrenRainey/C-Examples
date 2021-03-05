d64554aed28e10b2bc8b1a9162ae6728af3b252a
Darren Rainey <darren@darrenraineys.co.uk>
2021-03-05 02:37:31 +0000

    Fixed typo
    Uploaded wrong file version

--- kill-Process.c
+++ kill-Process.c
@@ -8,7 +8,7 @@ Created By Darren Rainey https://github.com/DarrenRainey
 
 int main(){
 	pid_t pid = [PROCESS ID]; // Pass or enter a process id to kill
-	kill(-killID, SIGTERM); // SIGTERM nicely tasks the program to die, SIGKILL force kills the process.
+	kill(-pid, SIGTERM); // SIGTERM nicely tasks the program to die, SIGKILL force kills the process.
 	
 	return 0;
 }
